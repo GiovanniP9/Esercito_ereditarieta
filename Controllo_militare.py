7cdb7c216178a8c9670e46b895cbbcca906cb411
GiovanniP9 <pisaniellogiovanni53@gmail.com>
2025-04-14 17:39:08 +0200

    modifica ad esercito e controllo militare + menu

--- Controllo_militare.py
+++ Controllo_militare.py
@@ -1,12 +1,10 @@
-from esercito import Esercito
-from unitaMilitare import Unita_militare
 from cavalleria import Cavalleria
 from fanteria import Fanteria
 from supportoLogistico import Supporto_logistico
 from ricognizione import Ricognizione
 from artiglieria import Artiglieria
 
-class Controllo_militare(Esercito, Fanteria, Cavalleria, Supporto_logistico, Ricognizione, Artiglieria):
+class Controllo_militare():
     def __init__(self):
         self.unita_registrate = {}
         
@@ -24,14 +22,35 @@ class Controllo_militare(Esercito, Fanteria, Cavalleria, Supporto_logistico, Ric
             return f"Nome: {unita.nome}, Numero Soldati: {unita.numero_soldati}"
         else:
             return "Unità non trovata."
+        
+    def attacca_unita(self, nome):
+        if nome in self.unita_registrate:
+            unita = self.unita_registrate[nome]
+            unita.attacca()
+        else:
+            print("Unità non trovata.")
+    def ritira_unita(self, nome):
+        if nome in self.unita_registrate:
+            unita = self.unita_registrate[nome]
+            unita.ritira()
+        else:
+            print("Unità non trovata.")
+    def muovi_unita(self, nome):
+        if nome in self.unita_registrate:
+            unita = self.unita_registrate[nome]
+            unita.muovi()
+        else:
+            print("Unità non trovata.")
+    
 
+# Creazione delle unità
 unita1 = Fanteria("Fanteria 1", 100)
 unita2 = Cavalleria("Cavalleria 1", 50)
 unita3 = Supporto_logistico("Supporto 1", 30)
 unita4 = Ricognizione("Ricognizione 1", 20)
 unita5 = Artiglieria("Artiglieria 1", 40)
 
-
+# Creazione dell'esercito
 controllo = Controllo_militare()
 controllo.aggiungi_unita(unita1)
 controllo.aggiungi_unita(unita2)
@@ -39,9 +58,22 @@ controllo.aggiungi_unita(unita3)
 controllo.aggiungi_unita(unita4)
 controllo.aggiungi_unita(unita5)
 
+# Mostra le unità registrate
 controllo.mostra_unita()
 controllo.dettagli_unita("Fanteria 1")
 controllo.dettagli_unita("Cavalleria 1")
 controllo.dettagli_unita("Supporto 1")
 controllo.dettagli_unita("Ricognizione 1")
 controllo.dettagli_unita("Artiglieria 1")
+
+# Attacca con un unità
+
+controllo.attacca_unita("Fanteria 1")
+
+# Ritira con un unità
+
+controllo.ritira_unita("Artiglieria 1")
+
+# Muove con un unità
+
+controllo.muovi_unita("Supporto 1")
